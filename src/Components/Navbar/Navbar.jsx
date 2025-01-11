7aa71448a11824ad41090730a5a2dce403dd1b20
Mehefuj Ali <mehefujalim@gmail.com>
2025-01-11 22:41:28 +0530

    responsive nav bar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,14 +2,15 @@ import { IoIosSearch } from "react-icons/io";
 import Logo from "../../Shaired/Logo";
 import { FiBell, FiShoppingCart } from "react-icons/fi";
 import { RiUser3Line } from "react-icons/ri";
+import { IoMenu } from "react-icons/io5";
 
 const Navbar = () => {
   return (
     <div className=" bg-primary-color  py-3 ">
-      <div className=" container mx-auto  flex justify-between items-center ">
+      <div className=" container mx-auto px-2  gap-12 flex justify-between items-center ">
         <Logo white={true}></Logo>
 
-        <div className="w-4/12  relative">
+        <div className=" md:w-4/12 w-full  relative">
           <IoIosSearch className=" absolute right-3 top-2 text-lg  " />
           <input
             placeholder="Search..."
@@ -18,20 +19,25 @@ const Navbar = () => {
           />
         </div>
 
-        <div className=" flex gap-7 select-none">
-          <div className="indicator">
-            <span className="indicator-item badge bg-white -top-2">3</span>
-            <button>
-              <FiBell className="text-2xl text-white" />
-            </button>
+        <div className=" flex items-center gap-3">
+          <div className="  hidden md:flex gap-7 select-none ">
+            <div className="indicator">
+              <span className="indicator-item badge bg-white -top-2">3</span>
+              <button>
+                <FiBell className="text-2xl text-white" />
+              </button>
+            </div>
+            <div className="indicator">
+              <span className="indicator-item badge bg-white -top-2">9</span>
+              <button>
+                <FiShoppingCart className="text-2xl text-white" />
+              </button>
+            </div>
+            <RiUser3Line className="text-2xl text-white cursor-pointer" />
           </div>
-          <div className="indicator">
-            <span className="indicator-item badge bg-white -top-2">9</span>
-            <button>
-              <FiShoppingCart className="text-2xl text-white" />
-            </button>
+          <div>
+            <IoMenu className=" text-2xl text-white cursor-pointer" />
           </div>
-          <RiUser3Line className="text-2xl text-white cursor-pointer" />
         </div>
       </div>
     </div>
--- src/Shaired/Logo.jsx
+++ src/Shaired/Logo.jsx
@@ -4,11 +4,11 @@ const Logo = ({ white }) => {
   return (
     <div className=" flex items-center gap-1">
       <img
-        className={`${white && "brightness-0 invert"} w-11`}
+        className={`${white && "brightness-0 invert"} w-6 md:w-11`}
         src="https://res.cloudinary.com/dkal0omka/image/upload/v1736597894/green-basket-logo_jrijx7.png"
         alt=""
       />
-      <h1 className={`${white?'text-white':'text-primary-color'}  text-2xl font-bold flex flex-col leading-none  `}>
+      <h1 className={`${white?'text-white':'text-primary-color'} text-sm md:text-xl   lg:text-2xl font-bold flex flex-col leading-none  `}>
         Green <span className=" font-light my-0">Basket</span>
       </h1>
     </div>
